1dc9649a78ca489494be520d2264cd083c67d866
ArSham446 <bcsm-f19-446@superior.edu.pk>
2023-07-11 10:58:04 +0500

    final touch

--- lib/home.dart
+++ lib/home.dart
@@ -3,317 +3,208 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    List<String> list = [
-      'AC',
-      'C',
-      '%',
-      '/',
-      '7',
-      '8',
-      '9',
-      '*',
-      '4',
-      '5',
-      '6',
-      '-',
-      '1',
-      '2',
-      '3',
-      '+',
-      '0',
-      '.',
-      '='
-    ];
-    List<String> lisT = [
-      'AC',
-      '1',
-      '2',
-      '3',
-      '*',
-      '/',
-      '4',
-      '5',
-      '6',
-      '7',
-      '+',
-      '-',
-      '8',
-      '9',
-      '0',
-      '.',
-      '%',
-      'C',
-      '='
-    ];
-    return MediaQuery.of(context).orientation == Orientation.portrait
-        ? Scaffold(
-            appBar: AppBar(
-              title: const Text('Calculator'),
-              centerTitle: true,
-            ),
-            body: Container(
-              padding: const EdgeInsets.all(20),
-              color: Colors.grey[300],
-              child: Column(
-                children: [
-                  Container(
-                    height: 160,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.blue[100],
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Colors.grey,
-                          blurRadius: 5,
-                          offset: Offset(0, 3),
-                        ),
-                      ],
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Consumer<CalculationsProvider>(
-                            builder: (context, calculationsProvider, child) =>
-                                Column(
-                              children: [
-                                Text(
-                                  calculationsProvider.firstNumber +
-                                      calculationsProvider.operator +
-                                      calculationsProvider.secondNumber,
-                                  style: const TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Text(
-                                  calculationsProvider.result,
-                                  style: const TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                const SizedBox(
-                                  height: 5,
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Expanded(
-                      child: SingleChildScrollView(
-                    scrollDirection: Axis.vertical,
-                    child: Wrap(
-                      direction: Axis.horizontal,
-                      children: List.generate(
-                        19,
-                        (index) {
-                          return Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: Container(
-                              width: list[index] == '=' ? 149 : 70,
-                              height: 70,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(20),
-                                boxShadow: const [
-                                  BoxShadow(
-                                    color: Colors.grey,
-                                    blurRadius: 5,
-                                    offset: Offset(0, 3),
-                                  ),
-                                ],
-                              ),
-                              child: Consumer<CalculationsProvider>(
-                                builder:
-                                    (context, calculationsProvider, child) =>
-                                        ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: list[index] == '='
-                                        ? Colors.blue[300]
-                                        : list[index] == 'AC' ||
-                                                list[index] == 'C' ||
-                                                list[index] == '%'
-                                            ? Colors.amber[300]
-                                            : list[index] == '/' ||
-                                                    list[index] == '*' ||
-                                                    list[index] == '-' ||
-                                                    list[index] == '+'
-                                                ? const Color.fromARGB(
-                                                    255, 4, 241, 151)
-                                                : Colors.white,
-                                    foregroundColor: list[index] == 'AC' ||
-                                            list[index] == 'C' ||
-                                            list[index] == '%'
-                                        ? Colors.green
-                                        : list[index] == '/' ||
-                                                list[index] == '*' ||
-                                                list[index] == '-' ||
-                                                list[index] == '+'
-                                            ? Colors.red
-                                            : Colors.black,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20),
-                                    ),
-                                  ),
-                                  onPressed: calculationsProvider.list[index],
-                                  child: Text(
-                                    list[index],
-                                    style: const TextStyle(fontSize: 20),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  )),
-                ],
-              ),
-            ))
-        //landscape
-        : Scaffold(
-            appBar: AppBar(
-              title: const Text('Calculator'),
-              centerTitle: true,
-            ),
-            body: Container(
-              padding: const EdgeInsets.all(20),
-              color: Colors.grey[300],
-              child: Row(
-                children: [
-                  Container(
-                    height: double.infinity,
-                    width: 250,
-                    decoration: BoxDecoration(
-                      color: Colors.blue[100],
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Colors.grey,
-                          blurRadius: 5,
-                          offset: Offset(0, 3),
-                        ),
-                      ],
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Consumer<CalculationsProvider>(
-                            builder: (context, calculationsProvider, child) =>
-                                Column(
-                              children: [
-                                Text(
-                                  calculationsProvider.history,
-                                  style: const TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Text(
-                                  calculationsProvider.result,
-                                  style: const TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                const SizedBox(
-                                  height: 5,
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 30,
-                  ),
-                  Expanded(
-                      child: SingleChildScrollView(
-                    scrollDirection: Axis.vertical,
-                    child: Wrap(
-                      direction: Axis.horizontal,
-                      children: List.generate(
-                        19,
-                        (index) {
-                          return Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: Container(
-                              width: lisT[index] == '=' ? 344 : 49,
-                              height: 49,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(20),
-                                boxShadow: const [
-                                  BoxShadow(
-                                    color: Colors.grey,
-                                    blurRadius: 5,
-                                    offset: Offset(0, 3),
-                                  ),
-                                ],
-                              ),
-                              child: Consumer<CalculationsProvider>(
-                                builder:
-                                    (context, calculationsProvider, child) =>
-                                        ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: lisT[index] == '='
-                                        ? Colors.blue[300]
-                                        : lisT[index] == 'AC' ||
-                                                lisT[index] == 'C' ||
-                                                lisT[index] == '%'
-                                            ? Colors.amber[300]
-                                            : lisT[index] == '/' ||
-                                                    lisT[index] == '*' ||
-                                                    lisT[index] == '-' ||
-                                                    lisT[index] == '+'
-                                                ? const Color.fromARGB(
-                                                    255, 4, 241, 151)
-                                                : Colors.white,
-                                    foregroundColor: lisT[index] == 'AC' ||
-                                            lisT[index] == 'C' ||
-                                            lisT[index] == '%'
-                                        ? Colors.green
-                                        : lisT[index] == '/' ||
-                                                lisT[index] == '*' ||
-                                                lisT[index] == '-' ||
-                                                lisT[index] == '+'
-                                            ? Colors.red
-                                            : Colors.black,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20),
-                                    ),
-                                  ),
-                                  onPressed: calculationsProvider.list[index],
-                                  child: Text(
-                                    lisT[index],
-                                    style: const TextStyle(fontSize: 20),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  )),
-                ],
-              ),
-            ));
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+    final buttonList = isPortrait ? _portraitButtonList : _landscapeButtonList;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculator'),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        color: Colors.grey[300],
+        child: isPortrait
+            ? _buildPortraitLayout(buttonList)
+            : _buildLandscapeLayout(buttonList),
+      ),
+    );
   }
+
+  Widget _buildPortraitLayout(List<String> buttonList) {
+    return Column(
+      children: [
+        _buildDisplay(),
+        const SizedBox(height: 20),
+        Expanded(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Wrap(
+              direction: Axis.horizontal,
+              children: List.generate(
+                buttonList.length,
+                (index) => _buildButton(buttonList[index], buttonList),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildLandscapeLayout(List<String> buttonList) {
+    return Row(
+      children: [
+        _buildDisplay(),
+        const SizedBox(width: 30),
+        Expanded(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Wrap(
+              direction: Axis.horizontal,
+              children: List.generate(
+                buttonList.length,
+                (index) => _buildButton(buttonList[index], buttonList),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDisplay() {
+    return Container(
+      height: 160,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.blue[100],
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Consumer<CalculationsProvider>(
+              builder: (context, calculationsProvider, child) => Column(
+                children: [
+                  Text(
+                    calculationsProvider.firstNumber +
+                        calculationsProvider.operator +
+                        calculationsProvider.secondNumber,
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    calculationsProvider.result,
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 5),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildButton(String label, List<String> buttonList) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Container(
+        width: label == '=' ? 149 : 70,
+        height: 70,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 5,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Consumer<CalculationsProvider>(
+          builder: (context, calculationsProvider, child) => ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: label == '='
+                  ? Colors.blue[300]
+                  : label == 'AC' || label == 'C' || label == '%'
+                      ? Colors.amber[300]
+                      : label == '/' ||
+                              label == '*' ||
+                              label == '-' ||
+                              label == '+'
+                          ? const Color.fromARGB(255, 4, 241, 151)
+                          : Colors.white,
+              foregroundColor: label == 'AC' || label == 'C' || label == '%'
+                  ? Colors.green
+                  : label == '/' || label == '*' || label == '-' || label == '+'
+                      ? Colors.red
+                      : Colors.black,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            onPressed: calculationsProvider.list[buttonList.indexOf(label)],
+            child: Text(
+              label,
+              style: const TextStyle(fontSize: 20),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  static const _portraitButtonList = [
+    'AC',
+    'C',
+    '%',
+    '/',
+    '7',
+    '8',
+    '9',
+    '*',
+    '4',
+    '5',
+    '6',
+    '-',
+    '1',
+    '2',
+    '3',
+    '+',
+    '0',
+    '.',
+    '=',
+  ];
+
+  static const _landscapeButtonList = [
+    'AC',
+    '1',
+    '2',
+    '3',
+    '*',
+    '/',
+    '4',
+    '5',
+    '6',
+    '7',
+    '+',
+    '-',
+    '8',
+    '9',
+    '0',
+    '.',
+    '%',
+    'C',
+    '=',
+  ];
 }
--- test/calculation_test.dart
+++ test/calculation_test.dart
@@ -1,16 +1,19 @@
 import 'package:calculator/calculations.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-
 void main() {
   group('CalculationsProvider', () {
     test('addition', () {
       final provider = CalculationsProvider();
-      provider.addDigit('2');
+      provider.addDigit('3');
+      provider.addOperator('+');
+      provider.addDigit('3');
+      provider.addOperator('+');
+      provider.addDigit('3');
       provider.addOperator('+');
       provider.addDigit('3');
       provider.calculate();
-      expect(provider.result, '5');
+      expect(provider.result, '12');
     });
 
     test('subtraction', () {
@@ -35,23 +38,23 @@ void main() {
       final provider = CalculationsProvider();
       provider.addDigit('6');
       provider.addOperator('/');
+      provider.addDigit('2');
+      provider.calculate();
+      expect(provider.result, '3');
+    });
+
+    test('modulus', () {
+      final provider = CalculationsProvider();
+      provider.addDigit('5');
+      provider.addOperator('%');
       provider.addDigit('3');
       provider.calculate();
       expect(provider.result, '2');
     });
 
-    test('modulus', () {
-      final provider = CalculationsProvider();
-      provider.addDigit('7');
-      provider.addOperator('%');
-      provider.addDigit('3');
-      provider.calculate();
-      expect(provider.result, '1');
-    });
-
     test('clear', () {
       final provider = CalculationsProvider();
-      provider.addDigit('2');
+      provider.addDigit('5');
       provider.addOperator('+');
       provider.addDigit('3');
       provider.clear();
@@ -64,14 +67,25 @@ void main() {
 
     test('deleteLastDigit', () {
       final provider = CalculationsProvider();
-      provider.addDigit('2');
+      provider.addDigit('5');
+      provider.addOperator('+');
       provider.addDigit('3');
+      provider.addDigit('5');
       provider.deleteLastDigit();
-      expect(provider.firstNumber, '2');
-      expect(provider.secondNumber, '');
+      expect(provider.secondNumber, '3');
+      provider.deleteLastDigit();
+      expect(provider.operator, '+');
+      provider.deleteLastDigit();
+      expect(provider.firstNumber, '5');
+      provider.deleteLastDigit();
+      expect(provider.firstNumber, '');
+      provider.deleteLastDigit();
       expect(provider.operator, '');
-      expect(provider.result, '');
-      expect(provider.history, '2');
+      provider.deleteLastDigit();
+      expect(provider.secondNumber, '');
     });
+
+    // Add more tests here...
+
   });
 }
